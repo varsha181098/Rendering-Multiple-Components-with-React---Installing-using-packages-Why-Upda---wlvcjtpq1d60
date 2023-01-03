3bed79550ab342e5e8168e4957a59737ecb618f8
varsha181098 <118925989+varsha181098@users.noreply.github.com>
2023-01-03 23:09:32 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,14 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
+import Description from "./Description";
+import Name from "./Name";
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <Name />
+      <Description />
+    </div>
   )
 }
 
