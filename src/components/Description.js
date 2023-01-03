cd4dc449fc851de9c8a43e4edb1ca9bc50cef8f7
varsha181098 <118925989+varsha181098@users.noreply.github.com>
2023-01-03 23:10:27 +0530

    Create Description.js

---
+++ src/components/Description.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Description() {
+    return (
+        <p data-ns-test="project-description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto facere amet assumenda aperiam ut eveniet eaque voluptatibus non libero, suscipit iste, voluptates, cupiditate dolorum doloribus enim ratione quisquam vitae? Nihil similique et, sed fuga eveniet deserunt, nulla aspernatur, nostrum enim temporibus explicabo pariatur adipisci perspiciatis ex exercitationem reiciendis eos. Consequatur?</p>
+    )
+
+}
+
+export default Description;
